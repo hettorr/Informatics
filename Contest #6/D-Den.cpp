98ccfa7ab58793b985e3109f60a49ef6e106b5af
hettorr <skpper23@gmail.com>
2025-11-20 22:44:47 +0200

    Add files via upload

--- Contest #6/D-Den.cpp
+++ Contest #6/D-Den.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 struct Bus {
-    int from, start, to, end;
+    int start, to, end;
 };
 
 struct Node {
@@ -13,49 +13,41 @@ struct Node {
     }
 };
 
-void readBuses(int routes, vector<Bus>& buses) {
+void readBuses(int routes, vector<vector<Bus>>& graph) {
     for (int i = 0; i < routes; i++) {
-        cin >> buses[i].from >> buses[i].start >> buses[i].to >> buses[i].end;
+        int from, start, to, end;
+        cin >> from >> start >> to >> end;
+        graph[from].push_back({start, to, end});
     }
 }
 
-int dijkstra(int towns, int start, int finish, vector<Bus>& buses) {
+int dijkstra(int towns, int start, int finish, vector<vector<Bus>>& graph) {
     vector<int> dist(towns + 1, INT_MAX);
     dist[start] = 0;
-    
     priority_queue<Node, vector<Node>, greater<Node>> pq;
     pq.push({0, start});
     
     while (!pq.empty()) {
         Node cur = pq.top();
         pq.pop();
-        
         if (cur.time <= dist[cur.city]) {
-            if (cur.city == finish) {
-                return cur.time;
-            }
-            
-            for (const Bus& bus : buses) {
-                if (bus.from == cur.city && bus.start >= cur.time) {
-                    if (bus.end < dist[bus.to]) {
-                        dist[bus.to] = bus.end;
-                        pq.push({bus.end, bus.to});
-                    }
+            if (cur.city == finish) return cur.time;
+            for (const Bus& bus : graph[cur.city]) {
+                if (bus.start >= cur.time && bus.end < dist[bus.to]) {
+                    dist[bus.to] = bus.end;
+                    pq.push({bus.end, bus.to});
                 }
             }
         }
     }
-    
     return -1;
 }
 
 int main() {
     int towns, start, finish, routes;
     cin >> towns >> start >> finish >> routes;
-    vector<Bus> buses(routes);
-    readBuses(routes, buses);
-    
-    int result = dijkstra(towns, start, finish, buses);
-    cout << result << "\n";
+    vector<vector<Bus>> graph(towns + 1);
+    readBuses(routes, graph);
+    cout << dijkstra(towns, start, finish, graph) << "\n";
     return 0;
 }
